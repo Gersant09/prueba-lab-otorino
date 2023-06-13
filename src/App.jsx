6c472bb83e7c445366840d013e99959b3afc4b93
Gerly Santiago <gerly.santiago@outlook.com>
2023-06-12 20:37:05 -0500

    prueba

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className='relative'>
+    <header className='max-w-[800px] mx-auto bg-gradient-to-t from-blue-700'>
+        <h1 className='text-3xl font-bold p-3 text-center gap-2 font-sans'>Otorrinolaringólogo en Monterrey</h1>
+      </header>
+      
+      
+
+      <p className=' p-3 text-center gap-6 max-w-[800px] mx-auto mt-10'>
+        El <strong>Jorge Treviño Garza</strong> es un <strong>Otorrinolaringólogo en Monterrey</strong> que cuenta
+        con especialidad en Cirugía Plástica Facial, es un médico que constantemente
+        se actualiza con el  fin de mejorar sus servicios a los pacientes.
+        Servicios del <strong>Otorrinolaringólogo en Monterrey</strong>
+      </p>
+
+      
+
+      <div className='p-5 text-center gap-3 max-w-[800px] mx-auto border-[2px] border-blue-500 mt-10 font-bold '>
+        <ul className='md:flex p-2 gap-2' >
+          <li className='cursor-pointer hover:text-blue-500 hover:text-xl   md:hover:text-white md:hover:bg-blue-500'>Consulta</li>
+          <li className='cursor-pointer hover:text-blue-500 hover:text-xl   md:hover:text-white md:hover:bg-blue-500'>Radiofrecuencia de Cornetes</li>
+          <li className='cursor-pointer hover:text-blue-500 hover:text-xl   md:hover:text-white md:hover:bg-blue-500'>Cirugia endoscópica para sinusitis</li>
+          <li className='cursor-pointer hover:text-blue-500 hover:text-xl   md:hover:text-white md:hover:bg-blue-500'>Cirugia de Tabique nasal</li>
+          <li className='cursor-pointer hover:text-blue-500 hover:text-xl   md:hover:text-white md:hover:bg-blue-500'>Endoscopia Nasal</li>
+          <li className='cursor-pointer hover:text-blue-500 hover:text-xl   md:hover:text-white md:hover:bg-blue-500'>Otorrinolaringología Pediátrica</li>
+        </ul>
+        
+      </div>
+
+      
+
+<div className='md:grid md:grid-cols-2'>
+<img className=' mt-10 gap-2 p-3 flex justify-center md:max-w-[450px] mx-auto' src="https://otorrinoencdmx.com/assets/img/seo/consulta-con-otorrinolaringologo-certificado.jpg" alt="otorrinolaringologo en monterrey" />
+
+      
+
+      <main className=' p-3 text-center gap-2 max-w-[800px] mx-auto mt-10 grid'>
+        <h2>
+          Te mereces un alto nivel de atención por parte de un <strong>Otorrinolaringólogo</strong> de vanguardia,
+          por lo que el <strong>Dr. Jorge Treviño Garza</strong> especialista en padecimiento de los oídos.
+        </h2>
+        <div>
+          Agenda una consulta por whatsapp al número  8115448285
+          o mediante una llamada al  8183475220
+          nuestro correo: jorgetrega@gmail.com
+          Lunes a Sábado
+          9:00 a 19:00 hrs
+        </div>
+        <div>
+          Visitanos para que seas atendido con la atención que te mereces en
+          Centro de Especialidades Médicas
+          1er piso consultorio 126 Jose Benitez No 2704 Colonia obispado Monterrey NL CP 64060
+        </div>
+      </main>
+      </div>
+      <button className='block mr-0 ml-auto text-green-700 text-5xl hover:text-6xl fixed bottom-0 right-0 md:bottom-[300px]'>
+        <i className='bx bxl-whatsapp text-right'></i>
+        </button>
+      <footer className=' p-3 text-center gap-2 max-w-[800px] mx-auto mt-10'>
+        <h3>
+          El <strong>otorrinolaringólogo en Monterrey Jorge Treviño</strong> es ahora uno de los médicos
+          más importantes en su campo gracias a su contribución a la investigación y la prevención.
+        </h3>
+        <p>
+          Da clic en el botón de WhatsApp para contactarnos para saber más sobre el <u className='hover:cursor-pointer text-blue-500'>otorrinolaringólogo en Monterrey.</u>
+          
+        </p>
+        
+        
+      </footer> 
+    </div>
+      
+      
+
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+
+    "./index.html",
+
+    "./src/**/*.{js,ts,jsx,tsx}",
+
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+
+        "hanken-grotesk": ['Hanken Grotesk', "sans-serif"]
+
+      }
+    },
+  },
+  plugins: [],
+}
+
